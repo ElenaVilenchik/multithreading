0e692f75234f174e96442a2f3f811899c304586c
User <User@User-PC>
2022-11-12 17:12:14 +0200

    HW - 39

--- multithreading/src/telran/multithreading/ThreadsRaceAppl.java
+++ multithreading/src/telran/multithreading/ThreadsRaceAppl.java
@@ -2,14 +2,16 @@ package telran.multithreading;
 
 public class ThreadsRaceAppl {
 
-	public static void main(String[] args) throws InterruptedException {
+	private static final int NUMBER_OF_THREADS = 5;
+	private static final int DISTANCE = 10;
 
+	public static void main(String[] args) throws InterruptedException {
 		ApplThread t = null;
-		for (int i = 0; i < 6; i++) {
-			t = new ApplThread(10);
+		for (int i = 0; i < NUMBER_OF_THREADS; i++) {
+			t = new ApplThread(DISTANCE);
 			t.start();
 		}
-		for (int i = 0; i < 6; i++) {
+		for (int i = 0; i < NUMBER_OF_THREADS; i++) {
 			t.join();
 		}
 	}
